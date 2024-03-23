aa35359292807276a0ab8c90fc25661dd336c29d
abobakaraslam <m.abobakar.aslam@gmail.com>
2024-03-23 23:23:30 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+/* File: App.jsx */
 function App() {
   return (
     <div>
       <h1 className="text-red-700">Author name</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
